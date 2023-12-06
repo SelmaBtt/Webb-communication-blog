a888944cefdb418849ace1db1af00d3b8886fa56
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2023-12-06 16:05:06 +0100

    comments

--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -1,21 +1,24 @@
 fetchBlogs()
-
+// Våran async funktion för att hämta all blogdata och sätta in i våran tabell
 async function fetchBlogs() {
     try {
+        // Sätta upp kontakt med vårat API och hämta datan. Då dessa variabler inte ska ändras kan vi använda const
         const response = await fetch('https://blog-api-assignment.up.railway.app/posts');
         const blogs = await response.json();
-        
+        // blogListHTML kommer vi fylla med våran HTML data i våran for loop, sedan sätta in i tabellen vi har i html
         let blogListHTML = "";
-        
+        // For loop för att gå igenom alla bloginlägg och lägga till datan i våran blogListHTML variabel
         for (let entry of blogs) {
+            // BlogDate är så vi lätt kan sätta in vårat datum i önskat format
             let blogDate = new Date(entry.date)
+            // Sen behöver vi dela upp våra taggar i en lista som vi vill sätta in i våran html. if satsen är så att vi endast går igenom och lägger till taggarna i våpran tagsHTML variabel så länge entry.tags inte är null, alltså avsett att det faktiskt finns en array där att gå igenom
             let tagsHTML = "";
-            if (entry.tags && entry.tags.length > 0) {
+            if (entry.tags!==null) {
                 for (let tag of entry.tags) {
                     tagsHTML += `<li>${tag}</li>`
                 }
             }
-
+            // Det är här vi skapar och sätter upp hur våran html ska se ut. vi sätter upp hur våran tabell ska se ut och sätter in datan från APiet
             blogListHTML += `
             <tr>
                 <td>${entry.title}</td>
@@ -29,7 +32,7 @@ async function fetchBlogs() {
             </tr>
             `
         }
-
+        // När loopen är klar så sätter vi in våran ihopsatta tabell i våran tabell body så ser vi allting från APiet
         document.getElementById('table-body').innerHTML = blogListHTML;
     } catch(error) {
         console.log(error)
